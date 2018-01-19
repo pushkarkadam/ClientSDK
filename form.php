53cd49b892d236cdbdf3fddd9690eda5f78191c7
Pushkar Kadam <pushkar.kadam@paydock.com>
2018-01-19 14:44:51 +1100

    modifications made in the form

--- form.php
+++ form.php
@@ -2,7 +2,6 @@
 Config::initialise("sandbox", "bf05dc59f637a712c900716c868ca7f9f231b3a7", "f4a3c72bb6bbccb3f612c30ac9496fbb16d2caec");
 
 $svc = new Charges();
-$res = $svc->create(100, "AUD")
-    ->withToken($token)
+$res = $svc->withToken($token)
     ->call();
 ?>
